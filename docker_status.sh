49f6543132e901c390996dea0cbf0b53f637e5ad
BigDavy123 <77096593+zsw79923@users.noreply.github.com>
2023-11-29 17:36:12 +0800

    Update docker_status.sh

--- docker_status.sh
+++ docker_status.sh
@@ -11,3 +11,15 @@ docker image ls
 # 列出所有容器
 echo "容器列表："
 docker container ls -a
+
+
+#============LOG============》
+#网络列表：
+#NETWORK ID     NAME      DRIVER    SCOPE
+#d39b58627ff5   bridge    bridge    local
+#a04e0e0e922b   host      host      local
+#3b1ee085f130   none      null      local
+#镜像列表：
+#REPOSITORY   TAG       IMAGE ID   CREATED   SIZE
+#容器列表：
+#CONTAINER ID   IMAGE     COMMAND   CREATED   STATUS    PORTS     NAMES
