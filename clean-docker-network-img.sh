26aa3e04d6c8b643c0b3b927841e38f6c5252a65
BigDavy123 <77096593+zsw79923@users.noreply.github.com>
2023-11-29 16:29:30 +0800

    Update clean-docker-network-img.sh

--- clean-docker-network-img.sh
+++ clean-docker-network-img.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# 停止并删除所有容器
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
 # 删除所有的 Docker 容器
 sudo docker rm -f $(sudo docker ps -aq)
 
@@ -8,6 +11,16 @@ sudo docker rmi -f $(sudo docker images -aq)
 
 # 删除所有的 Docker 网络
 sudo docker network prune -f
+
+# 获取所有无法删除的网络
+networks=$(docker network ls -qf "dangling=true")
+
+# 循环删除每个网络
+for network in $networks
+do
+    docker network rm $network
+done
+
 
 # 删除以 "docker" 开头的网络
 sudo ip link show | awk -F: '/docker/ {print $2}' | xargs -I {} sudo ip link set {} down
